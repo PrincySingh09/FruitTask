401db28dd2a9bcbb5a86053272e1dbaea3776c87
princysingh2409@gmail.com <princysingh2409@gmail.com>
2023-07-14 16:40:00 +0530

    Add project files.

---
+++ FruitTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList fruitList = new ArrayList();
+            fruitList.Add("apple");
+            fruitList.Add("banana");
+            fruitList.Add("cherry");
+            fruitList.Add("date");
+            fruitList.Add("elderberry");
+            Console.WriteLine("Total elements in ArrayList:" + fruitList.Count);
+            PrintElements(fruitList);
+            Console.WriteLine("\nArray list contain date:" + fruitList.Contains("date"));
+            fruitList.Insert(1, "fig");
+            PrintElements(fruitList);
+            Console.WriteLine("\nAfter inserting 'fig' at the second position:");
+            PrintElements(fruitList);
+            fruitList.Remove("banana");
+            Console.WriteLine("\nAfter removing banana:");
+            PrintElements(fruitList);
+            Console.WriteLine("\nSearch for apple:" + (fruitList.IndexOf("apple") != -1 ? "Found" : "Not Found"));
+            void PrintElements(ArrayList list)
+            {
+                Console.WriteLine("\nElements in ArrayList:");
+                foreach (string item in list)
+                {
+                    Console.WriteLine(item);
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
