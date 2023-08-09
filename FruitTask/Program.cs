4330aa34329a48fd3cab4e38174ce57c986f04fe
PrincySingh09 <139205001+PrincySingh09@users.noreply.github.com>
2023-08-09 15:16:13 +0530

    Update Program.cs

--- FruitTask/Program.cs
+++ FruitTask/Program.cs
@@ -39,3 +39,24 @@ namespace Task
                 Console.ReadKey();
             }
         }
+
+        import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PipeexComponent } from './pipeex/pipeex.component';
+import { BindingComponent } from './binding/binding.component';
+import { AnimalsComponent } from './animals/animals.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'pipeex', component: PipeexComponent },
+  { path: 'binding', component: BindingComponent },
+  { path: 'animals', component: AnimalsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
